2bc3ac7081415fdc6e28f4ad3bb896cde95230c0
Joseph <Josephmburumungai99@gmail.com>
2023-10-21 05:49:06 +0300

    Created pages and the Navbar component and also imported Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import AboutMe from "./pages/AboutMe"
+import Blogs from "./pages/Blogs"
+import ContactMe from "./pages/ContactMe"
+import Portfolio from "./pages/Portfolio"
+import Services from "./pages/Services"
 
 function App() {
   return (
-    <h1 className="text-red-700">App</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about-me" element={<AboutMe />} />
+      <Route path="/blogs" element={<Blogs />} />
+      <Route path="/contact-me" element={<ContactMe />} />
+      <Route path="/portfolio" element={<Portfolio />} />
+      <Route path="/services" element={<Services />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
